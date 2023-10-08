71f7c30825e5088e4449cb808352dcb2f3d9b9a3
KarolKotomski <karol.kotomski88@gmail.com>
2023-10-08 17:10:49 +0200

    Homepage Banner1 added and styled

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import Hero from "../components/Hero";
 import Booking from "../components/Booking";
 import { Plan } from "../components/Plan";
+import Banner1 from "../components/Banner1";
 
 const Home = () => {
 	return (
@@ -15,8 +16,9 @@ const Home = () => {
 				<Plan />
 			</section>
 			<section>Pick</section>
-			<section>Plan</section>
-			<section>Banner</section>
+			<section>
+				<Banner1 />
+			</section>
 			<section>Choose</section>
 			<section>Testimonials</section>
 			<section>Faq</section>
