d61bfa1b4388d0fba032248c8e0ffe891cb7a44b
KarolKotomski <karol.kotomski88@gmail.com>
2023-10-15 11:00:53 +0200

    Vehicles section dynamic data applied

--- src/components/VehiclesList.tsx
+++ src/components/VehiclesList.tsx
@@ -1,70 +1,23 @@
 import VehiclesModel from "./VehiclesModel";
 import audi from "../images/cars-big/audi-box.png";
+import { vehicles } from "../data/AllData";
 
 const VehiclesList = () => {
   return (
     <div className="container py-20">
       <div className="grid grid-cols-1 gap-10 p-5 sm:grid-cols-2 xl:grid-cols-3">
-        <VehiclesModel
-          image={audi}
-          brand="Audi"
-          model="A1"
-          price="$45"
-          door="2/3"
-          transmission="manual"
-          fuel="diesel"
-          line="S-Line"
-        />
-        <VehiclesModel
-          image={audi}
-          brand="Audi"
-          model="A1"
-          price="$45"
-          door="2/3"
-          transmission="manual"
-          fuel="diesel"
-          line="S-Line"
-        />
-        <VehiclesModel
-          image={audi}
-          brand="Audi"
-          model="A1"
-          price="$45"
-          door="2/3"
-          transmission="manual"
-          fuel="diesel"
-          line="S-Line"
-        />
-        <VehiclesModel
-          image={audi}
-          brand="Audi"
-          model="A1"
-          price="$45"
-          door="2/3"
-          transmission="manual"
-          fuel="diesel"
-          line="S-Line"
-        />
-        <VehiclesModel
-          image={audi}
-          brand="Audi"
-          model="A1"
-          price="$45"
-          door="2/3"
-          transmission="manual"
-          fuel="diesel"
-          line="S-Line"
-        />
-        <VehiclesModel
-          image={audi}
-          brand="Audi"
-          model="A1"
-          price="$45"
-          door="2/3"
-          transmission="manual"
-          fuel="diesel"
-          line="S-Line"
-        />
+        {vehicles.map((car) => (
+          <VehiclesModel
+            image={car.image1}
+            brand={car.brand}
+            model={car.model}
+            price={car.cost}
+            door={car.doors}
+            transmission={car.transmission}
+            fuel={car.fuel}
+            line={car.fuel}
+          />
+        ))}
       </div>
     </div>
   );
