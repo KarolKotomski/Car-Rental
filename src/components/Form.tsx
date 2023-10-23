b41bec990099b9a68a2341bb598fe48183aa4545
KarolKotomski <karol.kotomski88@gmail.com>
2023-10-23 20:31:39 +0200

    react-hook-form any yup library added. Form designed

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,169 +1,208 @@
-import { useId } from "react";
 import carIcon from "../icons/car_icon.png";
 import location from "../icons/location.png";
 import calendar from "../icons/calendar.png";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { DevTool } from "@hookform/devtools";
+
+const MIN_DATE = new Date();
+
+const schema = yup.object({
+  carType: yup.string().required("Car is required"),
+  pickLocation: yup.string().required("Pick-up location is required"),
+  dropLocation: yup.string().required("Drop-off location is required"),
+  pickDate: yup
+    .date()
+    .min(
+      MIN_DATE,
+      `Pick-up date cannot be ${MIN_DATE?.toLocaleDateString()} or earlier`,
+    )
+    .typeError("Pick-up date is required")
+    .required("Pick-up date is required"),
+  dropDate: yup
+    .date()
+    .min(
+      MIN_DATE,
+      `Drop-off date cannot be ${MIN_DATE?.toLocaleDateString()} or earlier`,
+    )
+    .when("pickDate", (pickDate, schema) => {
+      return (
+        pickDate &&
+        schema.min(
+          pickDate,
+          "Drop-off date cannot be earlier than pick-up date",
+        )
+      );
+    })
+    .typeError("Drop-off date is required")
+    .required("Drop-off date is required"),
+});
+
+type FormValues = {
+  carType: string;
+  pickLocation: string;
+  dropLocation: string;
+  pickDate: Date;
+  dropDate: Date;
+};
 
 const Form = () => {
-  const carTypeId = useId();
-  const pickLocationId = useId();
-  const dropLocationId = useId();
-  const pickDateId = useId();
-  const dropDateId = useId();
+  const form = useForm<FormValues>({
+    resolver: yupResolver(schema),
+  });
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
+
+  const onSubmit = (data: FormValues) => {
+    console.log("Form submitted", data);
+  };
 
   return (
-    <form className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
-      {/* Car type */}
-      <div className="rounded-md">
-        <div className="flex flex-col">
-          <div className="flex items-center gap-2">
-            <div>
-              <img alt="icon" src={carIcon} className="w-6"></img>
+    <>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3"
+      >
+        {/* Car type */}
+        <div className="rounded-md">
+          <div className="flex flex-col">
+            <div className="flex items-center gap-2">
+              <div>
+                <img alt="icon" src={carIcon} className="w-6"></img>
+              </div>
+              <div>
+                <label className="flex whitespace-nowrap py-3 text-lg font-medium">
+                  Select your car<span className="text-orange">*</span>
+                </label>
+              </div>
             </div>
             <div>
-              <label
-                htmlFor={carTypeId}
-                className="flex whitespace-nowrap py-3 text-lg font-medium"
+              <select
+                className="w-full rounded-md border border-[#ccd7e6] p-2"
+                {...register("carType")}
               >
-                Select your car<span className="text-orange">*</span>
-              </label>
+                <option value="">---</option>
+                <option value="">Audi A1</option>
+                <option value="">BMW 320</option>
+                <option value="">Mercedes-Benz GLK</option>
+                <option value="">Toyota Camry</option>
+                <option value="">Volkswagen Golf</option>
+                <option value="">Volkswagen Passat CC</option>
+              </select>
             </div>
-          </div>
-          <div>
-            <select
-              id={carTypeId}
-              name="carType"
-              className="w-full rounded-md border border-[#ccd7e6] p-2"
-            >
-              <option>---</option>
-              <option value="">Audi A1</option>
-              <option value="">BMW 320</option>
-              <option value="">Mercedes-Benz GLK</option>
-              <option value="">Toyota Camry</option>
-              <option value="">Volkswagen Golf</option>
-              <option value="">Volkswagen Passat CC</option>
-            </select>
+            <p className="mt-2 text-red-500">{errors.carType?.message}</p>
           </div>
         </div>
-      </div>
 
-      {/* Pick-up location */}
-      <div className="rounded-md">
-        <div className="flex flex-col">
-          <div className="flex items-center gap-2">
-            <div>
-              <img alt="icon" src={location} className="w-6"></img>
+        {/* Pick-up location */}
+        <div className="rounded-md">
+          <div className="flex flex-col">
+            <div className="flex items-center gap-2">
+              <div>
+                <img alt="icon" src={location} className="w-6"></img>
+              </div>
+              <div>
+                <label className="flex whitespace-nowrap py-3 text-lg font-medium">
+                  Pick-up location<span className="text-orange">*</span>
+                </label>
+              </div>
             </div>
             <div>
-              <label
-                htmlFor={pickLocationId}
-                className="flex whitespace-nowrap py-3 text-lg font-medium"
+              <select
+                className="w-full rounded-md border border-[#ccd7e6] p-2"
+                {...register("pickLocation")}
               >
-                Pick-up location<span className="text-orange">*</span>
-              </label>
+                <option value="">---</option>
+              </select>
             </div>
-          </div>
-          <div>
-            <select
-              id={pickLocationId}
-              name="pickLocation"
-              className="w-full rounded-md border border-[#ccd7e6] p-2"
-            >
-              <option value="">Audi A1</option>
-            </select>
+            <p className="mt-2 text-red-500">{errors.pickLocation?.message}</p>
           </div>
         </div>
-      </div>
 
-      {/* Drop-off location */}
-      <div className="rounded-md">
-        <div className="flex flex-col">
-          <div className="flex items-center gap-2">
-            <div>
-              <img alt="icon" src={location} className="w-6"></img>
+        {/* Drop-off location */}
+        <div className="rounded-md">
+          <div className="flex flex-col">
+            <div className="flex items-center gap-2">
+              <div>
+                <img alt="icon" src={location} className="w-6"></img>
+              </div>
+              <div>
+                <label className="flex whitespace-nowrap py-3 text-lg font-medium">
+                  Drop-off location<span className="text-orange">*</span>
+                </label>
+              </div>
             </div>
             <div>
-              <label
-                htmlFor={dropLocationId}
-                className="flex whitespace-nowrap py-3 text-lg font-medium"
+              <select
+                className="w-full rounded-md border border-[#ccd7e6] p-2"
+                {...register("dropLocation")}
               >
-                Drop-off location<span className="text-orange">*</span>
-              </label>
+                <option value="">---</option>
+              </select>
             </div>
-          </div>
-          <div>
-            <select
-              id={dropLocationId}
-              name="dropLocation"
-              className="w-full rounded-md border border-[#ccd7e6] p-2"
-            >
-              <option value="">Audi A1</option>
-            </select>
+            <p className="mt-2 text-red-500">{errors.dropLocation?.message}</p>
           </div>
         </div>
-      </div>
-      {/* Pick-up date */}
-      <div className="rounded-md">
-        <div className="flex flex-col">
-          <div className="flex items-center gap-2">
-            <div>
-              <img alt="icon" src={calendar} className="w-6"></img>
+        {/* Pick-up date */}
+        <div className="rounded-md">
+          <div className="flex flex-col">
+            <div className="flex items-center gap-2">
+              <div>
+                <img alt="icon" src={calendar} className="w-6"></img>
+              </div>
+              <div>
+                <label className="flex whitespace-nowrap py-3 text-lg font-medium">
+                  Pick-up date<span className="text-orange">*</span>
+                </label>
+              </div>
             </div>
             <div>
-              <label
-                htmlFor={pickDateId}
-                className="flex whitespace-nowrap py-3 text-lg font-medium"
-              >
-                Pick-up date<span className="text-orange">*</span>
-              </label>
+              <input
+                type="date"
+                className="w-full rounded-md border border-[#ccd7e6] px-2 py-[5.5px]"
+                {...register("pickDate")}
+              ></input>
             </div>
-          </div>
-          <div>
-            <input
-              id={pickDateId}
-              type="date"
-              name="pickDate"
-              className="w-full rounded-md border border-[#ccd7e6] px-2 py-[5.5px]"
-            ></input>
+            <p className="mt-2 text-red-500">{errors.pickDate?.message}</p>
           </div>
         </div>
-      </div>
 
-      {/* Drop-off date */}
-      <div className="rounded-md">
-        <div className="flex flex-col">
-          <div className="flex items-center gap-2">
-            <div>
-              <img alt="icon" src={calendar} className="w-6"></img>
+        {/* Drop-off date */}
+        <div className="rounded-md">
+          <div className="flex flex-col">
+            <div className="flex items-center gap-2">
+              <div>
+                <img alt="icon" src={calendar} className="w-6"></img>
+              </div>
+              <div>
+                <label className="flex whitespace-nowrap py-3 text-lg font-medium">
+                  Drop-off date<span className="text-orange">*</span>
+                </label>
+              </div>
             </div>
             <div>
-              <label
-                htmlFor={dropDateId}
-                className="flex whitespace-nowrap py-3 text-lg font-medium"
-              >
-                Drop-off date<span className="text-orange">*</span>
-              </label>
+              <input
+                type="date"
+                className="w-full rounded-md border border-[#ccd7e6] px-2 py-[5.5px]"
+                {...register("dropDate")}
+              ></input>
             </div>
-          </div>
-          <div>
-            <input
-              id={dropDateId}
-              type="date"
-              name="dropDate"
-              className="w-full rounded-md border border-[#ccd7e6] px-2 py-[5.5px]"
-            ></input>
+            <p className="mt-2 text-red-500">{errors.dropDate?.message}</p>
           </div>
         </div>
-      </div>
-      {/* Submit button */}
-      <div className="mt-5 flex items-end">
-        <button
-          type="submit"
-          className="h-10 w-full rounded bg-orange font-medium text-white shadow-lg shadow-orangeShadow transition-all duration-300 ease-in-out hover:opacity-80"
-        >
-          Search
-        </button>
-      </div>
-    </form>
+        {/* Submit button */}
+        <div className="mt-5 flex items-end">
+          <button
+            type="submit"
+            className="h-10 w-full rounded bg-orange font-medium text-white shadow-lg shadow-orangeShadow transition-all duration-300 ease-in-out hover:opacity-80"
+          >
+            Search
+          </button>
+        </div>
+      </form>
+      <DevTool control={control} />
+    </>
   );
 };
 
